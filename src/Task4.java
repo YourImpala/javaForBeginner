745b6d076efd6cb49553cff962f6dc5eba12ef34
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-09 20:28:11 +0300

    Task4

---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = Integer.parseInt(in.nextLine(), 2);
+        System.out.println(num);
+    }
+}
