66f2e3168b5d1c748090b24da56532b4fe8a98be
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-09 21:07:53 +0300

    Task6

---
+++ src/Task6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x, y, z;
+        System.out.println("Введите первое число");
+        x = in.nextInt();
+        System.out.println("Введите второе число");
+        y = in.nextInt();
+        System.out.println("Введите третье число");
+        z = in.nextInt();
+
+        double avg = (x + y + z) / 3.0;
+        System.out.println("Среднеарифмитическое = " + avg);
+        double result = avg / 2;
+        System.out.println("Деление средниарифмитического на 2 без остатка = " + result);
+        if(result > 3 )
+            System.out.println("Программа выполнена корректно");
+    }
+}
