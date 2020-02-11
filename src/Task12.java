6e11baab0353128b2870f7a5ef4f67a1a14f3bdd
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-11 06:22:00 +0300

    Task10

---
+++ src/Task12.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку с пробелами");
+        String str = sc.nextLine();
+        System.out.println("Строка очищенная от пробелов: \n" + str.replace(" ",""));
+    }
+}
