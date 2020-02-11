a880938e59f1f5883fd99420a4aed3470bd985ff
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-11 06:35:55 +0300

    Task13

---
+++ src/Task13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task13 {
+    public static void main(String[] args) {
+        String result;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первую строку");
+        String str1 = sc.nextLine();
+        System.out.println("Введите вторую строку");
+        String str2 = sc.nextLine();
+
+        if (str1.equals(str2)) {
+            System.out.println("Строки равны");
+        } else if (str1.length() > str2.length()) {
+            System.out.println("Длинная строка : "+ str1);
+        } else {
+            System.out.println("Длинная строка : " + str2);
+        }
+
+    }
+}
