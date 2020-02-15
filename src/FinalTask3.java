bc4cc205dbc55d090ff7dcf40be46317649748be
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-15 10:01:24 +0300

    FinalTask3

--- src/FinalTask3.java
+++ src/FinalTask3.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class FinalTask3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String rate;
+        String rubles;
+        double result;
+
+        do{
+            System.out.println("Введите текущий курс $:");
+            rate = sc.nextLine();
+        }while(!isNumeric(rate));
+        do{
+            System.out.println("Введите кол-во рублей:");
+            rubles = sc.nextLine();
+        }while(!isNumeric(rubles));
+        sc.close();
+
+        result = Double.parseDouble(rubles) / Double.parseDouble(rate);
+
+        System.out.println("Итого:");
+        System.out.printf("%.2f$", result);
+
+    }
+    private static boolean isNumeric(String str) {
+            try {
+                Double.parseDouble(str);
+                return true;
+            } catch(NumberFormatException e) {
+                return false;
+            }
+    }
 }
