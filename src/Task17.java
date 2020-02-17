88fd58e592ef851d50cc269f2f865e50e3f121a3
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-02-17 14:01:44 +0300

    now the stop word is not written to the file

--- src/Task17.java
+++ src/Task17.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Task17 {
     public static void main(String[] args) throws Exception{
-        String line = "";
         FileWriter fw = new FileWriter("src/files/textEx17");
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Наберите текст и нажмите 'enter', чтобы записать строку в файл.\nВведите 'stop', чтобы прекратить запись.");
-        while (!line.equals("stop")){
-            line = sc.nextLine();
+        while (true){
+            String line = sc.nextLine();
+            if(line.equals("stop")) break;
             fw.write(line + "\n");
         }
         fw.close();
